1ffd747e7e127ae7d595bc60e6caebbd91a2bcab
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-01-17 23:10:59 +0530

    Added a modified version

--- numguesserG.py
+++ numguesserG.py
@@ -23,8 +23,28 @@ while True:
 
  except ValueError:
    print("invalid input!")
+
+
+
    
- 
+ # -----Same but little modified.------  
+ '''import random
+number = random.randint(1,100)
+guess = int (input("guess the number: "))
+count=1
+if guess not in range(1,101):
+   print("invalid input!")
+   
+while guess!=number:
+    if guess<number:
+       print("guess higher")
+    else:
+       print("guess lower")
+    guess = int (input("guess the number: "))
+    count+=1
+else:
+   print("correct guess!")
+   print(f'number of guesses are {count}')'''
 
         
    
