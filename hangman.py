a19e26f8a299acf0f344794393180610bdc053c3
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-01-31 22:54:14 +0530

    updated hangman

--- hangman.py
+++ hangman.py
@@ -1,7 +1,17 @@
 import random
 
 def hangman():
-    words = ["python", "chess", "automation", "neon", "storytelling"]
+    words = ["python", "chess", "automation", "neon", "gang"]
     word = random.choice(words)
-    guessed = ["_"] * len(word)
+    guessed = ["_"]* len(word)
     attempts = 6
+
+    print("===Hangman===")
+    while attempts > 0 and "_" in guessed:
+        print ("word: "+" ".join(guessed))
+        guess = input("Guess a letter: ").lower()
+
+        
+
+
+
