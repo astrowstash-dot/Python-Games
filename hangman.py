1e14a5cd10587a5b6efb06d73bd657564592303c
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-01-31 23:03:09 +0530

    completed hangman game

--- hangman.py
+++ hangman.py
@@ -11,6 +11,23 @@ def hangman():
         print ("word: "+" ".join(guessed))
         guess = input("Guess a letter: ").lower()
 
+        if guess in word:
+            for i, letter in enumerate(word):
+                if letter == guess:
+                    guessed[i] = guess
+
+                else:
+                    attempts -= 1
+                    print(f'wrong! Attempts left: {attempts}')
+    if "_" not in guessed:
+        print(f'You guessed the word: {word}')
+    else:
+        print(f'you lost! The word was: {word}')
+
+if __name__ == "__main__":
+    hangman()     
+
+
         
 
 
