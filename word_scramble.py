f38fecbfe06790e94f2dcefa5658fd15ff5c5949
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-02-02 23:15:55 +0530

    changes made

--- word_scramble.py
+++ word_scramble.py
@@ -1,4 +1,11 @@
 import random
 
+def scramble_word(word):
+    """Return a scrambled version of the given word."""
+    word_letters = list(word)
+    random.shuffle(word_letters)
+    return ''.join(word_letters)
+
+
 # List of words to scramble
 WORDS = ["python", "developer", "hangman", "heisenberg", "mike", "heyyo", "saulgoodman", "breakingbad"]
