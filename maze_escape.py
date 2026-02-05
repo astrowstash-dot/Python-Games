dae72eb962671c3d47b23c2617d26da3350e1707
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-02-05 23:42:36 +0530

    update

--- maze_escape.py
+++ maze_escape.py
@@ -4,4 +4,6 @@ def maze_game():
     print("🏰 Welcome to the Maze Escape Game!")
     print("You are trapped in a maze. Find the exit!")
 
-   
+  
+    # Maze layout (E = exit, X = wall, . = path)
+    
